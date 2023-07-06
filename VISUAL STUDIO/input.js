021e6c6534488ec1020a6d1cf2cbb030aa6b335e
Prachi <117340825+D22IT212@users.noreply.github.com>
2023-07-06 11:10:15 +0530

    Add files via upload

---
+++ VISUAL STUDIO/input.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs=require("fs")
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.writeHead(200,'Content-Type', 'image/jpg');
+
+  
+data=fs.readFileSync('images.jpg') 
+    res.end(data,'binary');
+
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
---
+++ VISUAL STUDIO/server.js
@@ -0,0 +1,40 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  
+  function getSong() {
+    let _song = '';
+    let i = 100;
+    for (i; i > 0; i -= 1) {
+      _song += `${i} beers on the wall, you take one down and pass it around, ${
+        i - 1
+      } bottles of beer on the wall\n`;
+      if (i === 1) {
+        _song += "Hey let's get some more beer";
+      }
+    }
+  
+    return _song;
+  }
+  
+  function singSong(_song) {
+    if (!_song) throw new Error("song is '' empty, FEED ME A SONG!");
+    res.end(_song);
+  }
+  
+  const song = getSong();
+  // this will work
+  singSong(song);
+});
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+
+  
+
